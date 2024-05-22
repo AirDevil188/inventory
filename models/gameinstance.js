5c375d8de2ccdcf1c4eeea474d66650aa85c6792
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-05-22 15:52:39 +0200

    added schema for gameinstance model

---
+++ models/gameinstance.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GameInstanceSchema = new Schema({
+  game: { type: Schema.Types.ObjectId, ref: "Game", required: true },
+  platform: { type: String, required: true },
+  status: {
+    type: String,
+    enum: ["Maintenance", "Available", "Loaned", "Reserved"],
+    default: "Maintenance",
+  },
+  due_back: { type: Date, default: Date.now },
+});
+
+GameInstanceSchema.virtual("url").get(function () {
+  return `catalog/gameinstance/${this._id}`;
+});
+
+module.exports = mongoose.model("GameInstance", GameInstanceSchema);

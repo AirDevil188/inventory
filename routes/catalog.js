79b03e054af88afdd63e83495b1fdb236b89da5d
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-05-24 09:35:04 +0200

    created catalog route file and imported controllers

---
+++ routes/catalog.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const game_controller = require("../controllers/gameController");
+const publisher_controller = require("../controllers/publisherController");
+const developer_controller = require("../controllers/developerController");
+const genre_controller = require("../controllers/genreController");
+
+router.get("/", game_controller.index);
+
+module.exports = router;

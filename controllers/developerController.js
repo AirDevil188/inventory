1a261041da08a0f81fdbf1b9ea61cab08e787b04
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-05-27 20:59:44 +0200

    added two middleware fucntions for list_developers and developers_detail

---
+++ controllers/developerController.js
@@ -0,0 +1,34 @@
+const Game = require("../models/game");
+
+const Publisher = require("../models/publisher");
+const Developer = require("../models/developer");
+const Genre = require("../models/genre");
+const GameInstance = require("../models/gameinstance");
+
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+exports.list_developers = asyncHandler(async (req, res, next) => {
+  const allDevelopers = await Developer.find()
+    .sort({ name: 1 })
+    .populate("publisher")
+    .exec();
+
+  res.render("list_developers", {
+    title: "Developers List",
+    developer_list: allDevelopers,
+  });
+});
+
+exports.developer_detail = asyncHandler(async (req, res, next) => {
+  const [developer, allGames] = await Promise.all([
+    Developer.findById(req.params.id).exec(),
+    Game.find({ developer: req.params.id }).exec(),
+  ]);
+
+  res.render("developer_detail", {
+    title: developer.name,
+    developer: developer,
+    all_games: allGames,
+  });
+});

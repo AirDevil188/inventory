668e1cf5e5ac0f97cad5bd73d2577fc8c21aaada
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-05-28 12:54:19 +0200

    added routes for gameinstances

--- routes/catalog.js
+++ routes/catalog.js
@@ -5,6 +5,7 @@ const game_controller = require("../controllers/gameController");
 const publisher_controller = require("../controllers/publisherController");
 const developer_controller = require("../controllers/developerController");
 const genre_controller = require("../controllers/genreController");
+const gameinstance_controller = require("../controllers/gameInstanceController");
 
 router.get("/", game_controller.index);
 
@@ -23,5 +24,9 @@ router.get("/developer/:id", developer_controller.developer_detail);
 router.get("/genres", genre_controller.list_genres);
 
 router.get("/genre/:id", genre_controller.genre_detail);
+
+router.get("/gameinstances", gameinstance_controller.list_gameinstances);
+
+router.get("/gameinstance/:id", gameinstance_controller.gameinstance_detail);
 
 module.exports = router;

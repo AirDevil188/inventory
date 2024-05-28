8400f48176dce92e1e2feb9fef6e1fe3ce5337e5
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-05-28 17:21:03 +0200

    added two middleware functions for platform_list and platform_details

---
+++ controllers/platformController.js
@@ -0,0 +1,30 @@
+const Game = require("../models/game");
+const Platform = require("../models/platform");
+
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+exports.list_platforms = asyncHandler(async (req, res, next) => {
+  const allPlatforms = await Platform.find().sort({ name: 1 }).exec();
+
+  res.render("list_platforms", {
+    title: "List Platforms",
+    platform_list: allPlatforms,
+  });
+});
+
+exports.platform_detail = asyncHandler(async (req, res, next) => {
+  const [platform, allGamesPlatform] = await Promise.all([
+    Platform.findById(req.params.id).exec(),
+    Game.find({ platform: req.params.id }).sort({ title: 1 }).exec(),
+  ]);
+  if (platform === null) {
+    const err = new Error("Platform  was not found");
+    err.status = 404;
+    return next(err);
+  }
+  res.render("platform_detail", {
+    title: platform.title,
+    platform: platform,
+  });
+});

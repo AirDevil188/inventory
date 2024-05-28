82687813f706056fc0c2f2aa296cd077fb9383c7
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-05-28 17:23:09 +0200

    added routes for platform list and platform list details

--- routes/catalog.js
+++ routes/catalog.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const game_controller = require("../controllers/gameController");
 const publisher_controller = require("../controllers/publisherController");
 const developer_controller = require("../controllers/developerController");
+const platform_controller = require("../controllers/platformController");
 const genre_controller = require("../controllers/genreController");
 const gameinstance_controller = require("../controllers/gameInstanceController");
 
@@ -20,6 +21,10 @@ router.get("/publisher/:id", publisher_controller.publisher_detail);
 router.get("/developers", developer_controller.list_developers);
 
 router.get("/developer/:id", developer_controller.developer_detail);
+
+router.get("/platforms", platform_controller.list_platforms);
+
+router.get("/platform/:id", platform_controller.platform_detail);
 
 router.get("/genres", genre_controller.list_genres);
 

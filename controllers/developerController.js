f4fa3a886d5fc7540a3bca8e7327b60a04b5c6bf
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-05-30 22:46:19 +0200

    developer_form_get and developer_form_post middleware

--- controllers/developerController.js
+++ controllers/developerController.js
@@ -1,5 +1,6 @@
 const Game = require("../models/game");
 const Developer = require("../models/developer");
+const Publisher = require("../models/publisher");
 
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
@@ -34,3 +35,59 @@ exports.developer_detail = asyncHandler(async (req, res, next) => {
     all_games: allGames,
   });
 });
+
+exports.developer_form_get = asyncHandler(async (req, res, next) => {
+  const allPublishers = await Publisher.find().sort({ name: 1 }).exec();
+
+  console.log(allPublishers);
+
+  res.render("developer_form", {
+    title: "Create Developer",
+    list_publishers: allPublishers,
+  });
+});
+
+exports.developer_form_post = [
+  body("developer_name", "Name must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  body("developer_location", "Location must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  body(
+    "developer_date_of_foundation",
+    "Developer must have a date of foundation."
+  )
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const allPublishers = await Publisher.find().sort({ name: 1 }).exec();
+
+    const developer = new Developer({
+      name: req.body.developer_name,
+      publisher: req.body.developer_publisher,
+      location: req.body.developer_location,
+      date_of_foundation: req.body.developer_date_of_foundation,
+      date_of_closing: req.body.developer_date_of_closing,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("developer_form", {
+        title: "Create Developer",
+        developer: developer,
+        list_publishers: allPublishers,
+      });
+      return;
+    } else {
+      await developer.save();
+      res.redirect(developer.url);
+    }
+  }),
+];

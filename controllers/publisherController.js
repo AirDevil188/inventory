3d575013f48a6ba4e7288bda268db5d50cb670af
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-05-30 18:48:56 +0200

    added post and get middleware functions for publisher form and creating new publisher

--- controllers/publisherController.js
+++ controllers/publisherController.js
@@ -31,3 +31,49 @@ exports.publisher_detail = asyncHandler(async (req, res, next) => {
     all_games: allGamesByPublisher,
   });
 });
+
+exports.publisher_form_get = asyncHandler(async (req, res, next) => {
+  res.render("publisher_form", {
+    title: "Create Publisher",
+  });
+});
+
+exports.publisher_form_post = [
+  body("publisher_name", "Publisher name must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("publisher_location", "Publisher location must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body(
+    "publisher_date_of_foundation",
+    "Publisher must have a date of foundation."
+  )
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const publisher = new Publisher({
+      name: req.body.publisher_name,
+      location: req.body.publisher_location,
+      date_of_foundation: req.body.publisher_date_of_foundation,
+      date_of_closing: req.body.publisher_date_of_closing,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("publisher_form", {
+        title: "Create Publisher",
+        errors: errors.array(),
+        publisher: publisher,
+      });
+      return;
+    } else {
+      await publisher.save();
+      res.redirect(publisher.url);
+    }
+  }),
+];

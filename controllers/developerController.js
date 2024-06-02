3c6cba28e75759c7a83bca8a1f82fbd8906a4379
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-06-02 11:47:51 +0200

    added populate() in Developer query for publisher in developer_details middleware function. Added get aand post middleware functions for updating developer

--- controllers/developerController.js
+++ controllers/developerController.js
@@ -4,6 +4,7 @@ const Publisher = require("../models/publisher");
 
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
+const publisher = require("../models/publisher");
 
 exports.list_developers = asyncHandler(async (req, res, next) => {
   const allDevelopers = await Developer.find()
@@ -19,7 +20,7 @@ exports.list_developers = asyncHandler(async (req, res, next) => {
 
 exports.developer_detail = asyncHandler(async (req, res, next) => {
   const [developer, allGames] = await Promise.all([
-    Developer.findById(req.params.id).exec(),
+    Developer.findById(req.params.id).populate("publisher").exec(),
     Game.find({ developer: req.params.id }).exec(),
   ]);
 
@@ -129,3 +130,73 @@ exports.developer_delete_post = asyncHandler(async (req, res, next) => {
     res.redirect("/catalog/developers");
   }
 });
+
+exports.developer_update_get = asyncHandler(async (req, res, next) => {
+  const [developer, allPublishers] = await Promise.all([
+    Developer.findById(req.params.id).populate("publisher").exec(),
+    Publisher.find().sort({ title: 1 }).exec(),
+  ]);
+  if (developer === null) {
+    const err = Error("Developer was not found.");
+    err.status = 404;
+    return next(err);
+  }
+  console.log("ss");
+  console.log(developer.publisher._id, developer.publisher.id);
+
+  res.render("developer_form", {
+    title: "Update Developer",
+    developer: developer,
+    list_publishers: allPublishers,
+  });
+});
+
+exports.developer_update_post = [
+  body("developer_name", "Name must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  body("developer_location", "Location must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  body(
+    "developer_date_of_foundation",
+    "Developer must have a date of foundation."
+  )
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const developer = new Developer({
+      name: req.body.developer_name,
+      publisher: req.body.developer_publisher,
+      location: req.body.developer_location,
+      date_of_foundation: req.body.developer_date_of_foundation,
+      date_of_closing: req.body.developer_date_of_closing,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      const allPublishers = await Publisher.find().sort({ title: 1 }).exec();
+      res.render("developer_form", {
+        title: "Update Developer",
+        developer: developer,
+        list_publishers: allPublishers,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const updatedDeveloper = await Developer.findByIdAndUpdate(
+        req.params.id,
+        developer,
+        {}
+      );
+      res.redirect(updatedDeveloper.url);
+    }
+  }),
+];
